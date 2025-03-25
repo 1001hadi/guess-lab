6e635d962377a79a16b5542c9bbc5a6a07056736
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-25 13:17:03 -0600

    switch solution created, but has saml bug :)

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ let playerAnswer;
 
 for (let i = 0; i < 3; i++) {
   playerAnswer = Number(window.prompt("guess from 1 to 20"));
-  //   console.log(playerAnswer);
-  if (actualAnswer === playerAnswer) {
-    window.alert(`Good job ${actualAnswer} is the correct answer`);
-    break;
-  } else if (playerAnswer > actualAnswer) {
-    window.alert("too high");
-    // continue;
-  } else if (playerAnswer < actualAnswer) {
-    window.alert("too low");
+
+  // if (actualAnswer === playerAnswer) {
+  //   window.alert(`Good job ${actualAnswer} is the correct answer`);
+  //   break;
+  // } else if (playerAnswer > actualAnswer) {
+  //   window.alert("too high");
+  //   // continue;
+  // } else if (playerAnswer < actualAnswer) {
+  //   window.alert("too low");
+  // }
+  // if (i === 2) {
+  //   window.alert(`you missed, ${actualAnswer} was the answer!`);
+  // }
+
+  switch (true) {
+    case actualAnswer === playerAnswer:
+      window.alert(`Good job ${actualAnswer} is the correct answer`);
+      break;
+    case playerAnswer > actualAnswer:
+      window.alert("too high");
+      break;
+    case playerAnswer < actualAnswer:
+      window.alert("too low");
+      break;
   }
   if (i === 2) {
     window.alert(`you missed, ${actualAnswer} was the answer!`);
