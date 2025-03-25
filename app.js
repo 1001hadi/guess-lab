9a72deadec55e1d4e2616eb046c7be1d2de57d7d
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-25 12:09:30 -0600

    game logic writed and tested

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("This is a guessing Game!");
+window.alert(`Let's play the guessing game!`);
+
+const actualAnswer = 15;
+let playerAnswer;
+
+for (let i = 0; i < 3; i++) {
+  playerAnswer = Number(window.prompt("guess from 1 to 20"));
+  //   console.log(playerAnswer);
+  if (actualAnswer === playerAnswer) {
+    window.alert(`Good job ${actualAnswer} is the correct answer`);
+    break;
+  } else if (playerAnswer > actualAnswer) {
+    window.alert("too high");
+    // continue;
+  } else if (playerAnswer < actualAnswer) {
+    window.alert("too low");
+  }
+  if (i === 2) {
+    window.alert(`you missed, ${actualAnswer} was the answer!`);
+  }
+}
