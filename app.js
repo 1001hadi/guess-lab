9c02040b710f0f9ac0b20e47b87e7e3f48a9a0eb
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-25 15:20:29 -0600

    make sure the switch work correctly

--- app.js
+++ app.js
@@ -8,34 +8,34 @@ for (let i = 0; i < 5; i++) {
   // make sure player only enter number
   if (isNaN(playerAnswer)) window.alert("Please enter numbers between 1 - 100");
 
-  if (actualAnswer === playerAnswer) {
-    window.alert(`Good job ${actualAnswer} is the correct answer`);
-    break;
-  } else if (playerAnswer > actualAnswer) {
-    window.alert("too high");
-    // continue;
-  } else if (playerAnswer < actualAnswer) {
-    window.alert("too low");
+  // if (actualAnswer === playerAnswer) {
+  //   window.alert(`Good job ${actualAnswer} is the correct answer`);
+  //   break;
+  // } else if (playerAnswer > actualAnswer) {
+  //   window.alert("too high");
+  //   // continue;
+  // } else if (playerAnswer < actualAnswer) {
+  //   window.alert("too low");
+  // }
+
+  ////////
+  /////// With switch statement ---------------->
+
+  switch (true) {
+    case actualAnswer === playerAnswer:
+      window.alert(`Good job ${actualAnswer} is the correct answer`);
+      i = 5;
+      break;
+    case playerAnswer > actualAnswer:
+      window.alert("too high");
+      break;
+    case playerAnswer < actualAnswer:
+      window.alert("too low");
+      break;
   }
-  // if loop over let the user know the answer
+
+  //  if loop over let the user know the answer
   if (i === 4) {
     window.alert(`you missed, ${actualAnswer} was the answer!`);
   }
-
-  /////// With switch statement ---------------->
-  // switch (true) {
-  //   case actualAnswer === playerAnswer:
-  //     window.alert(`Good job ${actualAnswer} is the correct answer`);
-  //     i = 5;
-  //     break;
-  //   case playerAnswer > actualAnswer:
-  //     window.alert("too high");
-  //     break;
-  //   case playerAnswer < actualAnswer:
-  //     window.alert("too low");
-  //     break;
-  // }
-  // if (i === 4) {
-  //   window.alert(`you missed, ${actualAnswer} was the answer!`);
-  // }
 }
