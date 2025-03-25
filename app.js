d9c60aa55ea1d46ab4dcce501d86650dd590d758
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-25 14:55:07 -0600

    answer convert to random number

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
+// const body = document.querySelector("body");
+
 window.alert(`Let's play the guessing game!`);
 
-const actualAnswer = 15;
+const actualAnswer = Math.floor(Math.random() * 100);
+// console.log(actualAnswer);
+
 let playerAnswer;
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
   playerAnswer = Number(window.prompt("guess from 1 to 20"));
 
-  // if (actualAnswer === playerAnswer) {
-  //   window.alert(`Good job ${actualAnswer} is the correct answer`);
-  //   break;
-  // } else if (playerAnswer > actualAnswer) {
-  //   window.alert("too high");
-  //   // continue;
-  // } else if (playerAnswer < actualAnswer) {
-  //   window.alert("too low");
-  // }
-  // if (i === 2) {
-  //   window.alert(`you missed, ${actualAnswer} was the answer!`);
-  // }
-
-  switch (true) {
-    case actualAnswer === playerAnswer:
-      window.alert(`Good job ${actualAnswer} is the correct answer`);
-      i = 3;
-      break;
-    case playerAnswer > actualAnswer:
-      window.alert("too high");
-      break;
-    case playerAnswer < actualAnswer:
-      window.alert("too low");
-      break;
+  if (actualAnswer === playerAnswer) {
+    window.alert(`Good job ${actualAnswer} is the correct answer`);
+    break;
+  } else if (playerAnswer > actualAnswer) {
+    window.alert("too high");
+    // continue;
+  } else if (playerAnswer < actualAnswer) {
+    window.alert("too low");
   }
-  if (i === 2) {
+  if (i === 4) {
     window.alert(`you missed, ${actualAnswer} was the answer!`);
   }
+
+  // switch (true) {
+  //   case actualAnswer === playerAnswer:
+  //     window.alert(`Good job ${actualAnswer} is the correct answer`);
+  //     i = 5;
+  //     break;
+  //   case playerAnswer > actualAnswer:
+  //     window.alert("too high");
+  //     break;
+  //   case playerAnswer < actualAnswer:
+  //     window.alert("too low");
+  //     break;
+  // }
+  // if (i === 4) {
+  //   window.alert(`you missed, ${actualAnswer} was the answer!`);
+  // }
 }
